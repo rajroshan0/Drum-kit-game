eb00bf2cfefae5ead9f7651e3e212fd38c87969f
Roshan Kumar Paswan <111443249+rajroshan0@users.noreply.github.com>
2023-02-04 09:48:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+
+
+
+
+document.addEventListener("keypress",function(){
+  alert("key was pressed");
+}
+
+
+)
